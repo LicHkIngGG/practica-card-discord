855ba27750379d31bde3d8c18f183c6b908b1369
LichKinGgg <caperto303@gmail.com>
2023-11-01 18:52:12 -0400

    primer commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import { banner } from './componentes/banner'
+import './App.css'
+
+function App() {
+  
+
+  return (
+    
+      <div className='componente1'> 
+       <banner></banner>
+      </div>
+     
+  )
+}
+
+export default App
---
+++ src/componentes/banner.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './public/Banner _DDD.png'
+
+export const Banner = () => {
+
+return(
+    <div className='Banner'>
+        <img src = './public/Banner _DDD.png'alt ="">
+        </img>
+
+    </div>
+
+)
+
+}
